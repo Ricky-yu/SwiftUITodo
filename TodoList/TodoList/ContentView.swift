a5d9cc0b9ae1622ea7889e3792c3878dc99e2812
Ricky-yu <ts00484515@gmail.com>
2020-08-25 20:36:56 +0900

    set up PJ

---
+++ TodoList/TodoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by CHEN SINYU on 2020/08/25.
+//  Copyright © 2020 CHEN SINYU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
