c22b5159bf6a7b7d1a15821bfc8faabb946f8c22
Ricky-yu <ts00484515@gmail.com>
2020-08-26 00:07:50 +0900

    add AddButton

--- TodoList/TodoList/Home.swift
+++ TodoList/TodoList/Home.swift
@@ -17,7 +17,7 @@ class Main: ObservableObject {
     @Published var todos: [Todo] = []
     @Published var detailsShowing: Bool = false
     @Published var detailsTitle: String = ""
-    @Published var detialsDueDate: Date = Date()
+    @Published var detailsDueDate: Date = Date()
     
     func sort() {
         self.todos.sort(by: { $0.dueDate.timeIntervalSince1970 < $1.dueDate.timeIntervalSince1970})
@@ -29,13 +29,49 @@ class Main: ObservableObject {
 
 
 struct Home: View {
+    @ObservedObject var main: Main
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            TodoList(main: main)
+                .blur(radius: main.detailsShowing ? 10 : 0)
+            Button(action: {
+                editingMode = false
+                editingTodo = emptyTodoItem
+                detailShouldUpdateTitle = true
+                self.main.detailsTitle = ""
+                self.main.detailsDueDate = Date()
+                self.main.detailsShowing = true
+            }) {
+                btnAdd()
+            }.offset(x:UIScreen.main.bounds.width/2-60, y:UIScreen.main.bounds.height/2-80)
+            .blur(radius: main.detailsShowing ? 10 : 0)
+            TodoDetails(main: main)
+                .offset(x:0,y: main.detailsShowing ? 0 : UIScreen.main.bounds.height)
+        }
+    }
+}
+
+struct btnAdd: View {
+    var size: CGFloat = 65.0
+    var body: some View {
+        ZStack{
+            Group {
+                Circle()
+                    .fill(Color("btnAdd-bg"))
+            }.frame(width: self.size, height: self.size)
+                .shadow(color: Color("btnAdd-shadow"), radius: 10)
+            Group {
+                Image(systemName: "plus.circle.fill")
+                .resizable()
+                    .frame(width:self.size, height: self.size)
+                .foregroundColor(Color("theme"))
+            }
+        }
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Home()
+        Home(main: Main())
     }
 }
